984aa4a8fd5772c25a11e0c836949c81008932b8
Pump <patsanop@gmail.com>
2021-04-09 05:34:37 +0700

    backend implemented

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const User = require("./models/userSchema")
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-  res.render('index');
+mongoose.connect(
+  "mongodb+srv://testuser:testuserpass@cluster0.w9j5l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  res.redirect("/");
+});
+
+app.get("/user", (req, res) => {
+  res.render("user");
 });
 
 port = 3000;
---
+++ models/userSchema.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema({
+    fullname: String,
+    age: Number,
+    email: String,
+    idNumber: String,
+    carInfo: [String],
+  });
+
+module.exports = mongoose.model("User", schema);
+
