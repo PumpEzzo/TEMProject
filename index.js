10ad8656717edbb73f4eefcc5b973b5c55f47f5c
Pump <patsanop@gmail.com>
2021-04-15 23:06:35 +0700

    middleware implemented

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const path = require("path");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const User = require("./models/userSchema");
-const { request } = require("http");
-const { findById } = require("./models/userSchema");
-const { urlencoded } = require("express");
 const app = express();
 const flash = require("connect-flash");
 
@@ -13,10 +10,22 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(session({ secret: "Not good" }));
+app.use(session({ secret: "Not good", resave:true, saveUninitialized:true }));
 app.use(flash());
 
+function isLoggedIn(req, res, next) {
+  if (!req.session.user_id) {
+    res.redirect("/login");
+  }
+  next();
+}
 
+function isLoggedOut(req, res, next) {
+  if (!req.session.user_id) {
+    return next();
+  }
+  res.redirect("/user");
+}
 
 mongoose.connect(
   "mongodb+srv://testuser:testuserpass@cluster0.w9j5l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
@@ -27,19 +36,19 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/login", (req, res) => {
-  res.render("login",{message:req.flash("error")});
+app.get("/login", isLoggedOut, (req, res) => {
+  res.render("login", { message: req.flash("error") });
 });
 
 app.post("/login", async (req, res) => {
-  const {email, password} = req.body;
+  const { email, password } = req.body;
   //const user = new User(req.body);
   const foundUser = await User.validate(email, password);
   if (foundUser) {
     req.session.user_id = foundUser._id;
     res.redirect("/user");
   } else {
-    req.flash("error","username or password is incorrect");
+    req.flash("error", "username or password is incorrect");
     res.redirect("/login");
   }
 });
@@ -49,30 +58,24 @@ app.post("/logout", (req, res) => {
   res.redirect("/login");
 });
 
-app.get("/register", (req, res) => {
-  res.render("register",{message:flash("error")});
+app.get("/register", isLoggedOut, (req, res) => {
+  res.render("register", { message: flash("error") });
 });
 
 app.post("/register", async (req, res) => {
   const user = new User(req.body);
-  const valid = await User.findOne({email:user.email});
-  if(!valid){
+  const valid = await User.findOne({ email: user.email });
+  if (!valid) {
     await user.save();
     req.session.user_id = user._id;
     res.redirect("/user");
-  }
-  else{
-    req.flash("error","Email is already registered")
+  } else {
+    req.flash("error", "Email is already registered");
     res.redirect("/register");
   }
-  
-  
 });
 
-app.get("/user", async (req, res) => {
-  if (!req.session.user_id) {
-    res.redirect("/login");
-  }
+app.get("/user", isLoggedIn, async (req, res) => {
   const userFound = await User.findById(req.session.user_id);
   res.render("user", { userFound });
 });
